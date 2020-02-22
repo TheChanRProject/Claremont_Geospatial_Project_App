33ac4780119e05626df8b25dafcd6d444af420f8
TheChanRProject <garpchoo@gmail.com>
2020-02-22 00:04:52 -0800

    Commiting my changes

--- nearest_restaurants_schools_oop.py
+++ nearest_restaurants_schools_oop.py
@@ -3,13 +3,14 @@ import pandas as pd
 import googlemaps
 from haversine import haversine
 from os import listdir
-
+from random import choice
 
 
 class nearest_restaurants:
     def __init__(self, data_path):
+        api_keys = ["AIzaSyBHWiHNgsyEL8IzkG42rcZYmqzjIXXHswE", "AIzaSyCWg8Nc_PHzUa3QFPDuiJZbTtduSR0Oy1o", "AIzaSyA6_WJ3FFz275fXDdUDqbfAoCeJv-MgU3M", "AIzaSyA-uUIEY9yQlMxLyB1VvClmccmh0UhAF1I", "AIzaSyBEY4QQTyrSpkQ7zh7cLTAarT1XT5n7-OM", "AIzaSyCHHKIsjkmyc7Gl-3FlwDwvriIdolBNxm0", "AIzaSyChA2jpAdVygHY3t2_JgIpLVPf5Slddc_4", "AIzaSyBOBMgIgASl-f_ti76DAHrIwQDXRkoOSAU"]
         self.data_path = data_path
-        self.key = "AIzaSyBHWiHNgsyEL8IzkG42rcZYmqzjIXXHswE"
+        self.key =
         self.gmaps = googlemaps.Client(key = self.key)
         self.df = pd.read_csv(f"{self.data_path}")
 
